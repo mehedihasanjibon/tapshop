54c75ce28693d02cf25f27c98ccca275c8068b7f
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-19 19:39:38 +0600

    first card js code done

---
+++ script/allbutton.js
@@ -0,0 +1,19 @@
+let serial = 0
+// first card 
+document.getElementById("firstCard-btn").addEventListener("click", function(){
+    serial += 1;
+    const pName = document.getElementById("product-name").innerText;
+    const pPrice = document.getElementById("price").innerText;
+    const pQuantity = document.getElementById("Quantity").innerText;
+
+    // multiply 
+    const pTotalElement =parseInt(pPrice) * parseInt(pQuantity);
+
+    // show the data 
+    displayData(pName, pPrice, pQuantity, pTotalElement);
+    total(pTotalElement);
+    disableButton("firstCard-btn");
+});
+
+
+

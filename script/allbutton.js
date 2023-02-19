18c9010fdce43a86b0a038706a5d3f841edf5fc2
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-19 20:24:19 +0600

    compalte all code

--- script/allbutton.js
+++ script/allbutton.js
@@ -2,9 +2,9 @@ let serial = 0
 // first card 
 document.getElementById("firstCard-btn").addEventListener("click", function(){
     serial += 1;
-    const pName = document.getElementById("product-name").innerText;
-    const pPrice = document.getElementById("price").innerText;
-    const pQuantity = document.getElementById("Quantity").innerText;
+    const pName = document.getElementById("first-name").innerText;
+    const pPrice = document.getElementById("first-price").innerText;
+    const pQuantity = document.getElementById("first-Quantity").innerText;
 
     // multiply 
     const pTotalElement =parseInt(pPrice) * parseInt(pQuantity);
@@ -16,5 +16,69 @@ document.getElementById("firstCard-btn").addEventListener("click", function(){
 });
 
 // second card 
+document.getElementById("secondCard-btn").addEventListener("click", function(){
+    serial += 1;
+    const pName = document.getElementById("second-name").innerText;
+    const pPrice = document.getElementById("second-price").innerText;
+    const pQuantity = document.getElementById("second-Quantity").innerText;
 
+    // multiply 
+    const pTotalElement = parseInt(pPrice) * parseInt(pQuantity);
+
+    // show the data 
+    displayData(pName, pPrice, pQuantity, pTotalElement);
+    total(pTotalElement);
+    disableButton("secondCard-btn");
+});
+
+// third card 
+document.getElementById("thirdCard-btn").addEventListener("click", function(){
+    serial += 1;
+    const pName = document.getElementById("third-name").innerText;
+    const pPrice = document.getElementById("third-price").innerText;
+    const pQuantity = document.getElementById("third-Quantity").innerText;
+
+    // multiply 
+    const pTotalElement = parseInt(pPrice) * parseInt(pQuantity);
+
+    // show the data 
+    displayData(pName, pPrice, pQuantity, pTotalElement);
+    total(pTotalElement);
+    disableButton("thirdCard-btn");
+});
+
+// fourth card 
+document.getElementById("fourthCard-btn").addEventListener("click", function(){
+    serial += 1;
+    const pName = document.getElementById("fourth-name").innerText;
+    const pPrice = document.getElementById("fourth-price").innerText;
+    const pQuantity = document.getElementById("fourth-Quantity").innerText;
+
+    // multiply 
+    const pTotalElement = parseInt(pPrice) * parseInt(pQuantity);
+
+    // show the data 
+    displayData(pName, pPrice, pQuantity, pTotalElement);
+    total(pTotalElement);
+    disableButton("fourthCard-btn");
+});
+
+// fifth card 
+document.getElementById("fifthCard-btn").addEventListener("click", function(){
+    serial += 1;
+    const pName = document.getElementById("fifth-name").innerText;
+    const pPrice = document.getElementById("fifth-price").value;
+    const pQuantity = document.getElementById("fifth-Quantity").value;
+
+    const pTotalElement = parseInt(pPrice) * parseInt(pQuantity);
+
+    if(pPrice == "" || pQuantity == "" || pPrice <= 0 || pQuantity <= 0){
+        return alert("Please enter any valid number");
+    }
+
+    // show the data 
+    displayData(pName, pPrice, pQuantity, pTotalElement);
+    total(pTotalElement);
+    disableButton("fifthCard-btn");
+});
 
